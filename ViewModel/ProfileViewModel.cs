895696d7a45530083368d66689c69885ccbe8709
Ashok12341201 <asokkumarsusitharan@gmail.com>
2023-03-29 15:33:47 +0545

    mock server created

--- ViewModel/ProfileViewModel.cs
+++ ViewModel/ProfileViewModel.cs
@@ -2,17 +2,21 @@
 
 using EcommerceMAUI.Model;
 using EcommerceMAUI.Views;
+using System.ComponentModel;
 using System.Windows.Input;
+using System.Runtime.CompilerServices;
+//using static UIKit.UIGestureRecognizer;
+using System.Net.Http.Headers;
 
 namespace EcommerceMAUI.ViewModel
 {
-    public class ProfileViewModel : BaseViewModel
+    public class ProfileViewModel : BaseViewModel, INotifyPropertyChanged
     {
         public ICommand TapCommand { get; private set; }
 
         public string Name { get; set; } = "David Spade";
         public string Email { get; set; } = "iamdavid@gmail.com";
-        public string ImageUrl { get; set; } = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Avatar.png";
+        public string ImageUrl { get; set; } = "https://lh3.googleusercontent.com/a-/ACB-R5QQyGv67sQSwpeXl0P3jyn3qM2BsHtQZTsGfOrd=s88-w88-h88-c-k-no";
 
         public List<MenuItems> _MenuItems = new List<MenuItems>();
         public List<MenuItems> MenuItems
@@ -21,12 +25,38 @@ namespace EcommerceMAUI.ViewModel
             set { _MenuItems = value; }
         }
 
+        private string _membership;
+        public string Membership
+        {
+            get { return _membership; }
+            set
+            {
+                _membership = value;
+                OnPropertyChanged();
+            }
+        }
+       
+
         public ProfileViewModel()
         {
             PopulateData();
             CommandInit();
+            Membership = "Gold";
+            getMembership();
         }
 
+        public async Task getMembership()
+        {
+            HttpClient HttpClient = new HttpClient();
+            HttpClient.DefaultRequestHeaders.Add("accessToken", AuthenticationService.getAccessToken());
+            var response = await HttpClient.GetAsync("http://localhost:8070/authorized");
+            
+            response.EnsureSuccessStatusCode();
+            Membership = await response.Content.ReadAsStringAsync();
+
+          
+
+        }
         void PopulateData()
         {
             MenuItems.Clear();
@@ -43,5 +73,13 @@ namespace EcommerceMAUI.ViewModel
             });
 
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
