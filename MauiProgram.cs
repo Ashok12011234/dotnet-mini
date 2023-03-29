8cdb5921b41b0b29cd8683e2a3288b75693253f2
Hasini Samarathunga <hasinis@wso2.com>
2023-03-29 14:15:23 +0530

    Added logging

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,11 +1,19 @@
 ﻿//using CoreMidi;
 using EcommerceMAUI.Model.SDK;
+using log4net;
+
 namespace EcommerceMAUI;
 
 public static class MauiProgram
 {
+    static ILog log = LogManager.GetLogger(typeof(MauiProgram));
     public static MauiApp CreateMauiApp()
     {
+        //FileInfo fi = new FileInfo("log4net.config");
+        log4net.Config.XmlConfigurator.Configure();
+
+        log.Info("Hello hasini world!");
+
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
