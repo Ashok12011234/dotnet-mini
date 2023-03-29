7a01056e91840e50bb84a73f816be6b2ad31b853
Ashok12341201 <asokkumarsusitharan@gmail.com>
2023-03-29 11:41:08 +0545

    authntication service class created

---
+++ AuthenticationService.cs
@@ -0,0 +1,55 @@
+﻿//using CoreMidi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EcommerceMAUI.Model.SDK;
+using Newtonsoft.Json;
+
+namespace EcommerceMAUI;
+
+public partial class AuthenticationService
+{
+  
+    private static string _currentAccessToken;
+    private static AuthenticationHelper authenticationHelper;
+
+    public AuthenticationService(AuthenticationHelper _authenticationHelper)
+    {
+
+        authenticationHelper = _authenticationHelper;
+    }
+
+    public static async Task login()
+    {
+        authenticationHelper = new AuthenticationHelper();
+        await authenticationHelper.Login();
+        _currentAccessToken = authenticationHelper.AccessToken;
+
+    }
+
+    public static async Task logout()
+    {
+        await authenticationHelper.Logout(_currentAccessToken);
+        var request = authenticationHelper.Request;
+    }
+
+    public static string getAccessToken()
+
+    {
+        _currentAccessToken = authenticationHelper.AccessToken;
+        return _currentAccessToken;
+
+    }
+
+    public static string getUserInfo()
+    {
+        var userInfo = authenticationHelper.UserInfo;
+        dynamic json = JsonConvert.DeserializeObject(userInfo);
+        var subject = json.sub;
+        return subject;
+    }
+
+
+}
---
+++ ViewModel/LogoutViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EcommerceMAUI.Model.SDK;
+
+namespace EcommerceMAUI.ViewModel
+{
+    public class LogoutViewModel
+    {
+        public Command<object> RecommendedTapCommand { get; private set; }
+        public LogoutViewModel()
+        {
+            RecommendedTapCommand = new Command<object>(SelectRecommend);
+        }
+        private async void SelectRecommend(object obj)
+        {
+            // Authenticate the user
+            //AuthenticationHelper authenticationHelper = new AuthenticationHelper();
+            //await authenticationHelper.Login();
+            //var accessToken = authenticationHelper.AccessToken;
+            await AuthenticationService.logout();
+            //IdToken = authenticationHelper.AccessToken;
+            //Console.WriteLine(accessToken);
+            //IdToken = authenticationHelper.UserInfo;
+        }
+    }
+}
