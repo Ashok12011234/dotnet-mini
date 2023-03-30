51d0248188c689b70adc9e43c73d24d204721826
Davids-98 <dasithks@gmail.com>
2023-03-31 00:41:09 +0530

    devicesmediaadded

--- ViewModel/HomePageViewModel.cs
+++ ViewModel/HomePageViewModel.cs
@@ -57,10 +57,10 @@ namespace EcommerceMAUI.ViewModel
             CategoriesDataList.Add(new CategoriesModel() { CategoryID = 2, CategoryName = "Services", Icon = "\uf2cb" });
 
             BestSellingDataList.Clear();
-            BestSellingDataList.Add(new ProductListModel() { Name = "BeoPlay Speaker", BrandName = "Bang and Olufsen", Price = "$755", ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Image1.png" });
-            BestSellingDataList.Add(new ProductListModel() { Name = "Leather Wristwatch", BrandName = "Tag Heuer", Price = "$450", ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Image2.png" });
-            BestSellingDataList.Add(new ProductListModel() { Name = "Smart Bluetooth Speaker", BrandName = "Google LLC", Price = "$900", ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Image3.png" });
-            BestSellingDataList.Add(new ProductListModel() { Name = "Smart Luggage", BrandName = "Smart Inc", Price = "$1200", ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Image4.png" });
+            BestSellingDataList.Add(new ProductListModel() { Name = "Apple Iphone 14 Pro", BrandName = "Apple", Price = "$755", ImageUrl = "https://cellucity.co.za/wp-content/uploads/2022/09/Apple-iPhone-14-Pro-Max-5G-Deep-Purple.jpg" });
+            BestSellingDataList.Add(new ProductListModel() { Name = "Google Pixel 7 Pro", BrandName = "Google", Price = "$450", ImageUrl = "https://lh3.googleusercontent.com/2QKYQg6WuyEbx0Kd83N8-_lhs0Vk-GYQWCqsQjtuGejiimGFRTp-FEc9tDfMer_xoj7FaOMuAE7pg0Uj-rpWS9A2RpZiCQpZSXi3=rw-e365-w1050" });
+            BestSellingDataList.Add(new ProductListModel() { Name = "Samsung galaxy S23 Ultra", BrandName = "Samsung", Price = "$900", ImageUrl = "https://cdn.vox-cdn.com/uploads/chorus_asset/file/24396788/DSC04113_processed.jpg" });
+            BestSellingDataList.Add(new ProductListModel() { Name = "One Plus 11 5G", BrandName = "Google", Price = "$1200", ImageUrl = "https://fdn.gsmarena.com/imgroot/news/23/01/oneplus-11-ofic/inline/-1200/gsmarena_001.jpg" });
         }
 
         
